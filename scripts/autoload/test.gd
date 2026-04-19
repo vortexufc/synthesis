6e025a1c9e9e4e63652fca34c862ee361df9c53f
FranciscoAnderson2 <147929507+FranciscoAnderson2@users.noreply.github.com>
2026-04-19 17:17:42 -0300

    feat: conectadas novas questoes do supabase e visual da batalha corrigido
    - As 20 questões de biologia agora estão puxando certinho lá do nosso banco no Supabase.
    - O jogo agora sabe esperar que a internet traga as perguntas antes de começar a batalha
    - Atualizei a tela de Batalha: adicionei a letra E pra quem tiver 5 alternativas aparecer junto.
    - Dei uma espremida na tela (subi os personagens) pra fonte não vazar quando a questão tiver texto gigantesco estilo o do ENEM e deixei os botões divididos na tela em 2 coluninhas.
    - Tirei aquele arquivo de teste velho que tava quebrando ao rodar o jogo.

--- scripts/autoload/test.gd
+++ scripts/autoload/test.gd
@@ -3,9 +3,9 @@ extends Node
 func _ready():
 
 	print("teste começou")
-
-	for i in range(5):
-
-		var q = QuizManager.get_random_question()
-
-		print(q)
+	pass
+#	for i in range(5):
+#
+#		var q = QuizManager.get_random_question()
+#
+#		print(q)
